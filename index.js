46d0b5a85bcb56ad1505d5e99f34633213a2679c
Madeline Conway <conway.maddy@yahoo.com>
2024-07-30 12:26:58 -0400

    completed

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  "Hello!".toLowerCase(); // 'hello!'
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  "Hello!".toLowerCase(); // 'hello!'
+  function logShout(string){
+    console.log(string.toUpperCase());
+}
+"Hello!".toUpperCase(); // 'hello!'
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+    if (string === string.toLowerCase() ) {
+        return "I can't hear you!"
+    
+} 
+if(string === string.toUpperCase()) {
+    return "YES INDEED!"
+}if(string ==="Let's have dinner together!"){
+    return "I would love to!"
+}}
+
+
